7171a41cd5aed5ca088af847252650c1c8312304
haxibami <haxibami@gmail.com>
2020-08-11 00:31:02 +0900

    added new color themes

--- color.js
+++ color.js
@@ -2,7 +2,9 @@ var time= new Date();
 var hour = time.getHours();
 var hdr = document.getElementsByTagName('header');
 var link = document.getElementsByTagName('a');
+var acnt = document.getElementsByClassName('accent')
 
+/*Dawn*/
 if (hour<6) {
     document.body.style.backgroundColor ='#D9D7D7';
         hdr[0].style.backgroundColor ='#D90D32';
@@ -10,28 +12,35 @@ if (hour<6) {
         for(var i = 0; i < link.length; ++i){
             link[i].style.color ='#F28972';
         }
+        acnt[0].style.color ='#F28972';
 }
+/*Morning*/
 else if (hour<12) {
-    document.body.style.backgroundColor ='#D2D3D9';
-        hdr[0].style.backgroundColor ='#05C7F2';
+    document.body.style.backgroundColor ='#F2EA7E';
+        hdr[0].style.backgroundColor ='#049DD9';
         hdr[0].style.color ='white';
         for(var i = 0; i < link.length; ++i){
-            link[i].style.color ='#05DBF2';
+            link[i].style.color ='#F2D272';
         }
+        acnt[0].style.color ='#F29829';
 }
+/*Afternoon*/
 else if (hour<18) {
-    document.body.style.backgroundColor ='#BF9780';
-        hdr[0].style.backgroundColor ='#A61B40';
+    document.body.style.backgroundColor ='#F2E3D5';
+        hdr[0].style.backgroundColor ='#734058';
         hdr[0].style.color ='white';
         for(var i = 0; i < link.length; ++i){
-            link[i].style.color ='#590212';
+            link[i].style.color ='#D98566';
         }
+        acnt[0].style.color ='#F2505D';
 }
+/*Night*/
 else {
     document.body.style.backgroundColor ='#F2E7DC';
         hdr[0].style.backgroundColor ='#4E4873';
         hdr[0].style.color ='white';
         for(var i = 0; i < link.length; ++i){
-            link[i].style.color ='#F2ADA7';
+            link[i].style.color ='#F28157';
         }
+        acnt[0].style.color ='#F2ADA7';
 }
